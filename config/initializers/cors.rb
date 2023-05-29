29af2eb42aac7fab917d4ef14f17f9d56ab7dd91
T-berton <tberton@etud.insa-toulouse.fr>
2023-05-29 08:03:31 +0200

    env production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    if Rails.env.production?
+      origins "https://aidhub-openclassrooms.netlify.app"
+    else 
+      origins '*'
+    end
 
     resource "*",
       headers: :any,
