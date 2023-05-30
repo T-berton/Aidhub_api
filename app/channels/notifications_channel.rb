2e637075801e8c42919232b7c2c268c1ed946795
T-berton <tberton@etud.insa-toulouse.fr>
2023-05-30 05:27:14 +0200

    deleted channel

--- app/channels/notifications_channel.rb
+++
@@ -1,7 +0,0 @@
-# class NotificationsChannel < ApplicationCable::Channel
-#   def subscribed
-#     # stream_from "some_channel"
-#     stream_from "notifications_#{current_user.id}_#{params[:request_id]}"
-#   end
-
-# end
